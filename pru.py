1d4af139d9dcb125d9892f13e888365afe149e4b
Fernanda Amaral de Souza <fernanda_amaral@Decosdh.com>
2023-07-06 16:42:46 -0300

    pru

---
+++ pru.py
@@ -0,0 +1,9 @@
+print(" Digite um número inteiro : ")
+
+Answer = input ()
+
+string = str("PRU PRU PRU PRU PRU PRU PRU PRU PRU PRU PRU")
+
+AddFive = ()
+
+print (" Convertemos e " + str ( string ) ) 
